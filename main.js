129c38f9a0332f59dddecd6977e212108ecfde3a
Doniyortoshkentov05 <doniyortoshkentov@gmail.com>
2023-08-23 00:12:35 +0500

    end

---
+++ main.js
@@ -0,0 +1,167 @@
+
+//Masala 1 Masala 1 Masala 1 Masala 1 Masala 1 Masala 1 Masala 1 Masala 1 Masala 1 Masala 1 Masala 1 Masala 1 Masala 1
+
+// let k = 15;
+// let n = 4;
+
+// for (let i = 0; i <= n; i++) {
+//    console.log(k);
+// }
+
+
+// Masala 2 Masala 2 Masala 2 Masala 2 Masala 2 Masala 2 Masala 2 Masala 2 Masala 2 Masala 2 Masala 2 Masala 2 Masala 2
+
+// let a = 1;
+
+// let b = 24;
+
+// for (let i = a; i <= b; i++) {
+//    if (i % 2 == 0) {
+//       console.log();
+//    } else {
+      
+//    }
+// }
+// numbers(24 , 6)
+
+
+//    Masala 3 Masala 3 Masala 3 Masala 3 Masala 3 Masala 3 Masala 3 Masala 3 Masala 3 Masala 3 Masala 3 Masala 3 Masala 3
+// function numbers(a,b){
+//     for(let i = a-1;i>b ; i--){
+//         console.log(i);
+//     }
+// }
+// numbers(20 , 6)
+
+
+// Masala 4 Masala 4 Masala 4 Masala 4 Masala 4 Masala 4 Masala 4 Masala 4 Masala 4 Masala 4 Masala 4 Masala 4 Masala 4 Masala 4
+
+// let  =  10000;
+
+// for (let i = 0; i < 10; i++) {
+//    console.log(10000);   
+// }
+
+// Masala 5 Masala 5 Masala 5 Masala 5 Masala 5 Masala 5 Masala 5 Masala 5 Masala 5 Masala 5 Masala 5 Masala 5 Masala 5 Masala 5
+
+// let  a =  10000;
+
+// for (let i = 0.1 ; a - 0.1; i++) {
+//    console.log(a);   
+// }
+
+// Masala 6 Masala 6 Masala 6 Masala 6 Masala 6 Masala 6 Masala 6 Masala 6 Masala 6 Masala 6 Masala 6 Masala 6 Masala Masala 6 Masala 6
+
+// let a = 6;
+// let b = 5;
+// let c = 4;
+
+// for (let i = 0; 4 < 6 ; i++) {
+//    console.log(a,c);
+// }
+
+// Masala 7 Masala 7 Masala 7 Masala 7 Masala 7 Masala 7 Masala 7 Masala 7 Masala 7 Masala 7 Masala 7 Masala 7 Masala 7 Masala 7 Masala 7
+
+// let a = 7;
+// let b =6;
+
+// let Sum = 0
+// function sum(a,b){
+//     for(let i = a; i<b;i++){
+//         Sum+=i   
+//     }
+//     console.log(Sum)
+// }
+// sum(1,4)
+
+
+// Masala 8 Masala 8 Masala 8 Masala 8 Masala 8 Masala 8 Masala 8 Masala 8 Masala 8Masala 8 Masala 8 Masala 8 Masala 8 Masala 8 Masala 8
+
+// let a = 14;
+// let b = 19;
+// let c = 10;
+// let d = 4;
+
+// function musbat(a,b,c,d) {
+//    for (let i = a; i < b ; i++) {
+//       console.log(musbat);
+//    }
+// }
+
+// Masala 9 Masala 9 Masala 9 Masala 9 Masala 9 Masala 9 Masala 9 Masala 9 Masala 9 Masala 9 Masala 9 Masala 9 Masala 9Masala 9 Masala 9
+ 
+// let = Sum
+
+// function Sum(a,b,c) {
+//    for (let i = a; i < b; i++) {
+//       let pow = Math.pow(2,i)
+//       Sum+pow
+//    }
+//    console.log(Sum);
+// }
+// Sum = (5,3,7)
+
+// Masala 11 Masala 11 Masala 11 Masala 11 Masala 11 Masala 11 Masala 11 Masala 11 Masala 11 Masala 11 Masala 11 Masala 11 Masala 11 Masala 11
+ 
+// let n = 10;
+
+// for (let i = 1; i < n; i++) {
+//     if (i % 2 != 0) console.log(i);
+
+
+// Masala 12 Masala 12 Masala 12 Masala 12Masala 12  Masala 12 Masala 12 Masala 12 Masala 12 Masala 12 Masala 12 Masala 12 Masala 12 Masala 12
+
+// let n = 10
+// for (let i = 1; i < n; i++) {
+//     if (i % 2 == 0) console.log(i);
+
+// Masala 13 Masala 13 Masala 13 Masala 13 Masala 13 Masala 13 Masala 13 Masala 13 Masala 13 Masala 13 Masala 13 Masala 13 Masala 13 Masala 13
+
+// let n = 17;
+// for (let i = 0; i < n; i++) {
+//    if (i % 12 == 2) {
+//       console.log(i);
+//    } else {
+//       console.log(n);
+//    }
+// }
+
+// Masala 14 Masala 14 Masala 14 Masala 14 Masala 14 Masala 14 Masala 14 Masala 14 Masala 14 Masala 14 Masala 14 Masala 14 Masala 14 Masala 14 
+
+// let n = 20;
+
+// for (let i = 13; i < n; i++) {
+//    console.log(i);
+//    if (i + n) {
+//       console.log(n);
+//    } else {
+//       console.log(i);
+//    }
+// }
+
+// Masala 15 Masala 15 Masala 15 Masala 15Masala 15 Masala 15 Masala 15 Masala 15 Masala 15 Masala 15 Masala 15 Masala 15 Masala 15 Masala 15 Masala 15
+
+// let n =10;
+
+// function n(n) {
+//    for (let i = 0; i < n; i++) {
+//       console.log(n);
+//    }
+// }
+
+// Masala 16 Masala 16 Masala 16 Masala 16 Masala 16 Masala 16 Masala 16 Masala 16 Masala 16 Masala 16 Masala 16 Masala 16 Masala 16 Masala 16
+
+// let n = 5;
+// function betwen(n) {
+//    for (let i = 0; i == n; i++) {
+//       console.log(i);
+//    }
+// }
+
+// Masala 17 Masala 17 Masala 17 Masala 17 Masala 17 Masala Masala 17 Masala 17 Masala 17 Masala 17 Masala 17 Masala 17Masala 17 Masala 17 Masala 17
+
+// let n = 9;
+
+// for (let i = 2; i < n; i++) {
+//    console.log(i);
+// }
